7b18efbf7906919c72766a5f34360ea42cbab851
ThomasStuart <thomas.j.stuart96@gmail.com>
2020-07-10 00:29:43 -0700

    working but still missing some commit count numbers

--- scrapers.js
+++ scrapers.js
@@ -1,52 +1,89 @@
 const puppeteer = require('puppeteer');
-const fetch = require("node-fetch");
-
-async function scrapeProject(url){
-  const browser = await puppeteer.launch();
-  const page    = await browser.newPage();
-  await page.goto(url);
-
-  //For an image
-  // const [el1]  = await page.$x('');
-  // const imgSrc = await el1.getProperty('src');
-  // const srcTxt = await imgSrc.jsonValue();
-
-  const [el2]  = await page.$x('//*[@id="js-repo-pjax-container"]/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div[4]/ul/li[1]/a/span/strong');
-  const txt    = await el2.getProperty('textContent');
-  const rawTxt = await txt.jsonValue();
-
-  console.log({rawTxt});
-  browser.close();
-}
+const fetch     = require("node-fetch");
 
 
-async function getRepos(){
+async function getJsonObject(){
   let response = await fetch('https://api.github.com/users/ThomasStuart/repos');
-  let data     = await response.json()
+  let data     = await response.json();
+  //console.log(data);
   return data;
 }
 
-function collectRepoNamesFromUser(data){
+function parseJsonGetRepoNames(data){
   var repoNameList = [];
+
   for(var i = 0; i < data.length; i++){
       var name = data[i].name;
-      //repoNameList.push(name);
+      repoNameList.push(name);
       console.log("name: " + data[i].name);
   }
   return repoNameList;
 }
 
-function print( names ){
-  for(var i = 0; i < names.length; i++){
-      console.log("name: " + names );
+async function collectCommitCount( repoNames ){
+  let a = await scrapeCommitCounts(repoNames);
+  return a;
+}
+
+async function scrapeCommitCounts( repoNames ){
+  const browser = await puppeteer.launch();
+  console.log('In scraper function ');
+
+  var a = [];
+  for(var i = 0; i < repoNames.length; i++){
+      var name = repoNames[i];
+      console.log('name' + name);
+      var url  = 'https://github.com/ThomasStuart/' + name;
+      const page    = await browser.newPage();
+      await page.goto(url);
+      var rawTxt = 0;
+      
+      try{
+      // get a url
+      const [el]   = await page.$x('//*[@id="js-repo-pjax-container"]/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div[4]/ul/li[1]/a/span/strong');
+
+//*[@id="js-repo-pjax-container"]/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div[4]/ul/li[1]/a/span/strong
+//*[@id="js-repo-pjax-container"]/div[2]/div/div[2]/div[1]/div[3]/div[1]/div/div[4]/ul/li[1]/a/span/strong // MyFirstPullRequest
+//*[@id="js-repo-pjax-container"]/div[2]/div/div[2]/div[1]/div[3]/div[1]/div/div[4]/ul/li[1]/a/span/strong //March20
+///html/body/div[4]/div/main/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div[4]/ul/li[1]/a/span/strong
+      const txt    = await el.getProperty('textContent');
+      rawTxt = await txt.jsonValue();
+      console.log({rawTxt});
+      //console.log('name:' + name + ', count: ' + rawTxt );
+      //a.push([name, rawTxt]);
+    }
+    catch(err){
+      console.log(err.message);
+      try{
+        const [el]   = await page.$x('//*[@id="js-repo-pjax-container"]/div[2]/div/div[2]/div[1]/div[3]/div[1]/div/div[4]/ul/li[1]/a/span/strong');
+        const txt    = await el.getProperty('textContent');
+        rawTxt = await txt.jsonValue();
+        console.log({rawTxt});
+      }
+      catch{
+        console.log(err.message);
+        rawTxt = 0;
+      }
+    }
+    finally{
+        a.push([name, rawTxt]);
+    }
+
+      //console.log('name:' + name + ', count: ' + {rawTxt});
+  }
+  //console.log({rawTxt});
+
+  return a;
+}
+
+function print( a ){
+  console.log('print() method executed');
+  for(var i = 0; i < a.length; i++){
+      console.log("name: " + a[i][0] + ", commits: ", a[i][1] );
   }
 }
 
-//scrapeProject('https://github.com/ThomasStuart/prework-repo');
-//getRepos().then(data => console.log(data));
-getRepos().then( data => collectRepoNamesFromUser( data ) );
-
-// same as above, but response.json() parses the remote content as JSON
-fetch('/article/promise-chaining/user.json')
-  .then(response => response.json())
-  .then(user => alert(user.name)); // iliakan, got user name
+getJsonObject()
+  .then( allJson     => parseJsonGetRepoNames( allJson ) )
+  .then( repoNames   => collectCommitCount( repoNames )   );
+  // .then( a           =>  print( a) );
